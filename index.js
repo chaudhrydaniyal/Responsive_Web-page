3464df5638562a49e4b4b307fe65c7c4264a3e17
daniyal-khalid <daniyalkhalid@yahoo.com>
2021-12-20 18:52:26 +0500

    basic-project

---
+++ index.js
@@ -0,0 +1,36 @@
+$('#fullscreen').click(function (e) {
+    
+    $('#div-to-expand').toggleClass('fullscreen');
+    $('#div-to-expand').hasClass("fullscreen") ? $('#btn1').attr("src","minimize.svg") : $('#btn1').attr('src',"maximize.svg");
+});
+
+$('#fullscreen-2').click(function (e) {
+    $('#div-to-expand-2').toggleClass('fullscreen');
+    $('#div-to-expand-2').hasClass("fullscreen") ? $('#btn2').attr("src","minimize.svg") : $('#btn2').attr('src',"maximize.svg");
+});
+
+
+$('#fullscreen-3').click(function (e) {
+    $('#div-to-expand-3').toggleClass('fullscreen');
+    $('#div-to-expand-3').hasClass("fullscreen") ? $('#btn3').attr("src","minimize.svg") : $('#btn3').attr('src',"maximize.svg");
+});
+
+$('#fullscreen-4').click(function (e) {
+    $('#div-to-expand-4').toggleClass('fullscreen');
+    $('#div-to-expand-4').hasClass("fullscreen") ? $('#btn4').attr("src","minimize.svg") : $('#btn4').attr('src',"maximize.svg");
+});
+
+$('#fullscreen-5').click(function (e) {
+    $('#div-to-expand-5').toggleClass('fullscreen');
+    $('#div-to-expand-5').hasClass("fullscreen") ? $('#btn5').attr("src","minimize.svg") : $('#btn5').attr('src',"maximize.svg");
+});
+
+$('#fullscreen-6').click(function (e) {
+    $('#div-to-expand-6').toggleClass('fullscreen');
+    $('#div-to-expand-6').hasClass("fullscreen") ? $('#btn6').attr("src","minimize.svg") : $('#btn6').attr('src',"maximize.svg");
+});
+
+$('#fullscreen-7').click(function (e) {
+    $('#div-to-expand-7').toggleClass('fullscreen');
+    $('#div-to-expand-7').hasClass("fullscreen") ? $('#btn7').attr("src","minimize.svg") : $('#btn7').attr('src',"maximize.svg");
+});
